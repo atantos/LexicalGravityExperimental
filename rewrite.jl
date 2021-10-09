a2e808dbfe6ab13be65a186210c7cf6cbeaa0465
Alex Tantos <atantos@users.noreply.github.com>
2021-10-10 00:55:48 +0300

    get_ngrams_dictionary for the Dictionary type
    Here is the version for using the Dictionary data type. Essentially, I used the Dictionary's API for tokens and indices and replaced the respective rows of your initial version. Check out the results.md for benchmarking differences between the two.

--- rewrite.jl
+++ rewrite.jl
@@ -27,6 +27,29 @@ function get_ngrams(str::S, window_size::Int) where {S <: AbstractString}
     return bigram_tokens
 end
 
+# Here is the version for using the Dictionary data type. Essentially, I used the Dictionary's API for tokens and indices.
+function get_ngrams_dictionary(str::S, window_size::Int) where {S <: AbstractString}
+    str = replace(str, r"([?!.])\s" => Base.SubstitutionString("\\1\n"))
+    str = WordTokenizers.postproc_splits(str)
+    
+    bigram_tokens = Dictionary{String, Int}()
+    
+    for sentence in split(str, '\n')
+        words = tokenize(sentence)
+        max_i = length(words) - window_size + 1
+        for i in 1:max_i
+            ngram = join(words[i:(i + window_size - 1)], " ")
+            j = gettoken(bigram_tokens, ngram)[2][2]
+            if j > 1
+                @inbounds Dictionaries._values(bigram_tokens)[j] += 1
+            else
+                @inbounds set!(bigram_tokens, ngram, 1)
+            end
+        end
+    end
+    return bigram_tokens
+end
+
 function get_ngrams_alt(str::S, window_size::Int) where {S <: AbstractString}
     str = replace(str, r"([?!.])\s" => Base.SubstitutionString("\\1\n"))
     str = WordTokenizers.postproc_splits(str)
