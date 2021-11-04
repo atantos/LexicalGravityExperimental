e1a7bb0e27a75f99dac497c81ee880f397fb845e
Jake Ireland <jakewilliami@icloud.com>
2021-11-04 23:28:50 +1300

    Added Proof-of-Concept function using a DataFrame as a dictionary In our other DataFrame function, we store the data in a dictionary until the end of the loop, at which point we convert it to a DataFrame.  This function aims to avoid allocating the temporary dictionary.
    As expected, it turns out that dictionaries are much faster at key-lookups than for-loops over DataFrame rows, as that is their job...so this function is not very fast at all.

--- rewrite.jl
+++ rewrite.jl
@@ -2,7 +2,7 @@ using WordTokenizers
 using Dictionaries
 using DataFrames
 
-include("utils.jl")
+include("utils.jl") # this file contains code for `eachsplit` while we wait for 1.8 to come out...
 
 # The big_text.txt is on a separate file you of the repo.
 s = read("big_text.txt", String);
@@ -52,11 +52,51 @@ function get_ngrams_dataframe(str::S, window_size::Int) where {S <: AbstractStri
         end
     end
     
-    bigram_tokens_df = DataFrame([(String[] for _ in 1:(window_size + 1))...,  Int[]], :auto)
-    rename!(bigram_tokens_df, [:context, (Symbol("w$i") for i in 1:window_size)..., :count])
+    bigram_tokens_df = DataFrame(Vector{Union{String, Int}}[(String[] for _ in 1:(window_size + 1))...,  Int[]], :auto)
+    rename!(bigram_tokens_df, Symbol[:context, (Symbol("w$i") for i in 1:window_size)..., :count])
     
     for (tokens, _count) in bigram_tokens
-        push!(bigram_tokens_df, [join(tokens, ' '), tokens..., _count])
+        push!(bigram_tokens_df, (join(tokens, ' '), tokens..., _count))
+    end
+    
+    return bigram_tokens_df
+end
+
+# This one uses a dataframe as a dictionary, so it is not very efficient...at all...
+# it is more of a proof of concept than anything useful; I was wondering how a 
+# dataframe would do if it were acting as a dictionary.
+function get_ngrams_dataframe_alt(str::S, window_size::Int) where {S <: AbstractString}
+    str = replace(str, r"([?!.])\s" => Base.SubstitutionString("\\1\n"))
+    # Replacing the commas with spaces.
+    str = replace(str, r"([a-z\s]*),([\sa-z]+)" => Base.SubstitutionString("\\1\\2"))
+    str = WordTokenizers.postproc_splits(str)
+    
+    ncols = window_size + 2
+    bigram_tokens_df = DataFrame(Vector{Union{String, Int}}[(String[] for _ in 2:ncols)...,  Int[]], :auto) # 2:ncols == 1:(ncols - 1) == 1:(window_size + 1)
+    rename!(bigram_tokens_df, Symbol[:context, (Symbol("w$i") for i in 1:window_size)..., :count])
+    
+    for sentence in eachsplit(str, '\n')
+        words = tokenize(sentence)
+        max_i = length(words) - window_size + 1
+        for i in 1:max_i
+            ngram = ntuple(j -> words[i + j - 1], window_size)
+            ngram_str = join(ngram, ' ')
+            # this method is probably not efficient, but we essentially use the dataframe as a dictionary
+            # in this loop, we check that the current ngram is not in the dataframe
+            # it _might_ be faster to check the last row first...but not entirely sure
+            ngram_in_df = false
+            for j in nrow(bigram_tokens_df):-1:1
+                if bigram_tokens_df[j, :context] == ngram_str
+                    ngram_in_df = true
+                    bigram_tokens_df[j, :count] += 1
+                    break
+                end
+            end
+            if !ngram_in_df
+                # we have not found this ngram in the dataframe, so we need to add it
+                push!(bigram_tokens_df, (ngram_str, ngram..., 1))
+            end
+        end
     end
     
     return bigram_tokens_df
@@ -82,27 +122,6 @@ function get_ngrams_dictionary(str::S, window_size::Int) where {S <: AbstractStr
             end
         end
     end
-    return bigram_tokens
-end
-
-function get_ngrams_alt(str::S, window_size::Int) where {S <: AbstractString}
-    str = replace(str, r"([?!.])\s" => Base.SubstitutionString("\\1\n"))
-    str = WordTokenizers.postproc_splits(str)
     
-    bigram_tokens = Dict{NTuple{window_size, String}, Int}()
-    
-    for sentence in eachsplit(str, '\n')
-        words = tokenize(sentence)
-        max_i = length(words) - window_size + 1
-        for i in 1:max_i
-            ngram = ntuple(j -> words[i + j - 1], window_size)
-            k = Base.ht_keyindex2!(bigram_tokens, ngram)
-            if k > 0
-                @inbounds bigram_tokens.vals[k] += 1
-            else
-                @inbounds Base._setindex!(bigram_tokens, 1, ngram, -k)
-            end
-        end
-    end
     return bigram_tokens
 end
